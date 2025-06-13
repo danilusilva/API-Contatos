8b43135dfd9276db4d5e6b2fa926501c6d7be5bd
= <=>
2025-06-12 21:17:58 -0300

    add retorno listar , add put add delete

--- routes/contatoRoutes.js
+++ routes/contatoRoutes.js
@@ -3,10 +3,18 @@ const router = express.Router()
 const Contatos = require('../models/Contato');
 const { default: mongoose } = require("mongoose");
 
+
 // Listar contatos
 router.get('/', async (req, res) => {
     const dados = await Contatos.find();
-    res.json(dados)
+
+    const retorno = {
+        "alert": dados.length == 0 ? 'warning' : 'success',
+        "message": "Dados listados com sucesso.",
+        "data": dados
+    }
+
+    res.json(retorno)
 })
 
 // Criar contato
@@ -18,8 +26,8 @@ router.post('/', async (req, res) => {
 //Buscar por ID
 router.get('/:id', async (req, res) => {
     try {
-        if(!mongoose.Types.ObjectId.isValid(req.params.id)){
-            return res.status(404).json({"error": "Id enviado não é válido"})
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(404).json({ "error": "Id enviado não é válido" })
         }
         const dado = await Contatos.findById(req.params.id);
         if (!dado) {
@@ -28,7 +36,35 @@ router.get('/:id', async (req, res) => {
         res.status(200).json(dado)
     }
     catch (err) {
-        res.status(404).json({"error": err.message})
+        res.status(404).json({ "error": err.message })
+    }
+})
+
+//Atualizar por ID
+router.put('/:id', async (req, res) => {
+    try {
+        const dado = await Contatos.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!dado) {
+            return res.status(404).json({ error: 'Contato não encontrado' })
+        }
+        res.json(dado)
+    }
+    catch (err) {
+        res.status(404).json({ "error": err.message })
+    }
+})
+
+//deletar por ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const dado = await Contatos.findByIdAndDelete(req.params.id)
+        if (!dado) {
+            return res.status(404).json({ error: 'Contato não encontrado' })
+        }
+        res.json({ message: 'Contato deletado com sucesso!' })
+    }
+    catch (err) {
+        res.status(404).json({ "error": err.message })
     }
 })
 
